61a7941ca208fbd197845b3ce7c110642dbb1dd1
bhakareahan <ahanabhakare@gmail.com>
2019-09-30 15:21:04 -0500

    add button

--- app/src/main/java/com/example/myahan/MainActivity.java
+++ app/src/main/java/com/example/myahan/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.myahan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button helloButton;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloButton = findViewById(R.id.button);
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                helloButton.setText("Count: " + counter);
+            }
+        });
     }
 }
